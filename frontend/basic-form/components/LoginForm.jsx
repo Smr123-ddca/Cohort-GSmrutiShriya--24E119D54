a5cc21962de4cc907cf108f5717d6426377d818b
G Smruti Shriya <gsmrutishriya12@gmail.com>
2026-07-31 09:30:20 +0530

    made similar structured login page and profile page

---
+++ frontend/basic-form/components/LoginForm.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+
+function Form() {
+  const [formData, setFormData] = useState({
+    fullname: "",
+    reg_id: "",
+    email: "",
+    password: "",
+    age: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({...formData,[e.target.name]: e.target.value,});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+    const response = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      alert(result.message);
+      return;
+    }
+
+    alert("Loggedin successfully!");
+  } catch (error) {
+    console.error(error);
+    alert("Could not reach the server");
+  }
+};
+    
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Registration Form</h2>
+
+      {/* <label>Full Name:</label>
+      <input
+        type="text"
+        name="fullname"
+        value={formData.fullname}
+        onChange={handleChange}
+      />
+
+      <br /><br /> */}
+
+      {/* <label>Registration ID:</label>
+      <input
+        type="text"
+        name="reg_id"
+        value={formData.reg_id}
+        onChange={handleChange}
+      />
+
+      <br /><br /> */}
+
+      <label>Email:</label>
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+
+      <br /><br />
+
+      <label>Password:</label>
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+
+      <br /><br />
+
+      {/* <label>Age:</label>
+      <input
+        type="number"
+        name="age"
+        value={formData.age}
+        onChange={handleChange}
+      />
+
+      <br /><br /> */}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+
+};
+
+export default Form;
